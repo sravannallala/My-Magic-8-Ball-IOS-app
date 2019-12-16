3710bf086aebbf01338d1545436c999e4526efa6
Sravan Nallala <50337910+sravannallala@users.noreply.github.com>
2019-12-16 11:33:14 +0100

    Add files via upload

---
+++ Magic-8-Ball/Magic-8-Ball/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Magic-8-Ball
+//
+//  Created by sravan on 22/11/2019.
+//  Copyright © 2019 SravanNallala. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello potti am so sorry")
+            .padding(3.0)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
